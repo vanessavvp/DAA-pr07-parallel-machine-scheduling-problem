88195b6c5b7f6222c8f549d5e82c954415d8b051
Vanessa Villalba <alu0101265704@ull.edu.es>
2021-04-11 13:44:39 +0100

    Adding principle files and directories

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+/**
+ * PROJECT HEADER
+ * @file main.cc
+ * @author: Vanessa Valentina Villalba Perez
+ * Contact: alu0101265704@ull.edu.es
+ * @date: 11/04/2021
+ * Subject: Diseño y Análisis de Algoritmos
+ * Practice: Numberº7
+ * Purpose: Parallel Machine Scheduling Problem with Dependent Setup Times
+ */
+
+int main(int argc, char* argv) {
+  
+}
